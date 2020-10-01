e76fafe2d6227b638e42d1bef4ec02fbe4630278
Ubuntu <ubuntu@ip-10-0-0-141.ap-south-1.compute.internal>
2020-10-01 11:51:04 +0000

    demo2 update with nginx script

--- demo2/instance.tf
+++ demo2/instance.tf
@@ -1,3 +1,4 @@
+
 resource "aws_key_pair" "logginkey" {
   key_name   = "mykey"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
@@ -7,14 +8,16 @@ resource "aws_instance" "example"{
 ami = var.AMIS[var.AWS_REGION]
 instance_type = "t2.micro"
 key_name      = aws_key_pair.logginkey.key_name
-}
+
 
 provisioner "file" {
     source      = "script.sh"
     destination = "/tmp/script.sh"
   }
 
-  provisioner "remote-exec" {
+	
+
+provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/script.sh",
       "sudo sed -i -e 's/\r$//' /tmp/script.sh",  # Remove the spurious CR characters.
@@ -22,7 +25,7 @@ provisioner "file" {
     ]
   }
 
-  connection {
+connection {
     host        = coalesce(self.public_ip, self.private_ip)
     type        = "ssh"
     user        = var.INSTANCE_USERNAME
--- demo2/var.tf
+++ demo2/var.tf
@@ -21,8 +21,8 @@ type = string
 default = "mykey.pub"
 }
 
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
 }
 
 variable "INSTANCE_USERNAME" {
